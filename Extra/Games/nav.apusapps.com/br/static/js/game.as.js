90b0437127c0e0d85521a870a04759fee975a6dc
AcademicFun <157843674+AcademicAce@users.noreply.github.com>
2024-03-13 17:52:49 -0800

    Create game.as.js

---
+++ Extra/Games/nav.apusapps.com/br/static/js/game.as.js
@@ -0,0 +1,16 @@
+var str=
+'<div id="botBox">'+
+'    <div id="spn" style="text-align:center">'+
+'        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>'+
+'        <!-- mobile-game -->'+
+'        <ins class="adsbygoogle"'+
+'             style="display:inline-block;width:320px;height:50px"'+
+'             data-ad-client="ca-pub-4776032073028713"'+
+'             data-ad-slot="6641027382"></ins>'+
+'        <script>'+
+'        (adsbygoogle = window.adsbygoogle || []).push({});'+
+'        </script>'+
+'    </div>'+
+'    <div id="botBoxClose">x</div>'+
+'</div>';
+document.write(str);
